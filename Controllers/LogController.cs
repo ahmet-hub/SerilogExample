5b25d42860085e50dbd43a63efc824cd986859bd
ahmet yardımcı <ahmetyardimci98@gmail.com>
2022-04-18 15:34:21 +0300

    Add project files.

---
+++ Controllers/LogController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using SerilogTimings;
+
+namespace SerilogExample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LogController : ControllerBase
+    {
+        private readonly ILogger<LogController> _logger;
+        public LogController(ILogger<LogController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("Error")]
+        public IActionResult LogError(string value)
+        {
+            try
+            {
+                var test = value[10];
+                return Ok(value);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogAppError(ex, "extension-error");
+                //Log.Error(ex.ToString());
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("Timer")]
+        public IActionResult LogTime(int DocumentId)
+        {
+            using (var op = Operation.Begin("{DocumentId} is downloading", DocumentId))
+            {
+                Thread.Sleep(500);
+                op.Abandon();
+                op.Complete();
+
+                return Ok();
+            }
+        }
+
+        [HttpPost("Masking")]
+        public IActionResult LogMask(User user)
+        {
+            try
+            {
+                Login(user);
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                Log.Warning("Login failed {@User}", new User { Username = user.Username, Password = user.Password });
+                //_logger.LogAppWarning("Login failed {@User}");
+                return BadRequest();
+            }
+        }
+
+        public class User
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+
+        private void Login(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LogExtension.cs
@@ -0,0 +1,24 @@
+﻿using Serilog.Context;
+using System.Runtime.CompilerServices;
+
+namespace SerilogExample
+{
+	public static class LoggerExtensions
+	{
+		public static void LogAppError(this ILogger logger, Exception exception, string message, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
+		{
+			using var prop = LogContext.PushProperty("Method", memberName);
+			LogContext.PushProperty("FilePath", sourceFilePath);
+			LogContext.PushProperty("LineNumber", sourceLineNumber);
+			logger.LogError(exception, message);
+		}
+
+		public static void LogAppWarning(this ILogger logger, string message, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
+		{
+			using var prop = LogContext.PushProperty("Method", memberName);
+			LogContext.PushProperty("FilePath", sourceFilePath);
+			LogContext.PushProperty("LineNumber", sourceLineNumber);
+			logger.LogWarning(message);
+		}
+	}
+}
